44d38b692ee764c6686e2f61f36f6e55a836c3d5
Rebwar Hosein Poori <rebwar.poori@hotmail.com>
2020-06-14 12:31:22 +0430

    Add project files.

---
+++ UsingNullReferenceType.WithoutNull/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UsingNullReferenceType.WithoutNull
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Rebwar", "Poori");
+            Person person1 = new Person("Rebwar", "Hosein", "Poori");
+            Console.WriteLine($"Welcome, {person.Name} {person.LastName}");
+            //Console.WriteLine($"The Length of {person.Middle}={GetMiddleLength(person)}");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine($"Welcome, {person1.Name} {person1.Middle} {person1.LastName}");
+            Console.WriteLine($"The Length of {person1.Middle}={GetMiddleLength(person1)}");
+
+        }
+        static int GetMiddleLength(Person person)
+        {
+            string middleName = person.Middle;
+            return middleName.Length;
+        }
+    }
+}
